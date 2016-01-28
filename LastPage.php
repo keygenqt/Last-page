685d5b28176d4ad10f6be9177839b1fc5b2166de
KeyGenQt <keygenqt@gmail.com>
2016-01-28 11:15:45 +0300

    update

---
+++ LastPage.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace keygenqt\lastPage;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * LastPage controller
+ */
+class LastPage extends Controller
+{
+    public function beforeAction($action)
+    {
+        $result = parent::beforeAction($action);
+        if (empty($_FILES)) {
+            $request = Yii::$app->request;
+            if ((!$request->isAjax || !empty($_GET['ajax'])) && strpos($request->getUrl(), '.') === false) {
+                if (($oldUrl = Yii::$app->session->get('user.url')) !== null) {
+                    if (($newUrl = Yii::$app->session->get('user.new-url')) != $request->getUrl()) {
+                        Yii::$app->session->set('user.new-url', $request->getUrl());
+                        Yii::$app->session->set('user.url', $newUrl);
+                    }
+                } else {
+                    Yii::$app->session->set('user.new-url', $request->getUrl());
+                    Yii::$app->session->set('user.url', '/');
+                }
+                Yii::$app->user->setReturnUrl(Yii::$app->session->get('user.url'));
+            }
+        }
+        ActiveAssets::register($this->getView());
+        return $result;
+    }
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# yii2-last-page
+yii2-last-page
+===================
+
+LastPage lets you get last url.
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either add
+
+```
+"require": {
+    "keygenqt/yii2-last-page": "*"
+},
+"repositories":[
+    {
+        "type": "git",
+        "url": "https://github.com/keygenqt/yii2-last-page.git"
+    }
+]
+```
+
+of your `composer.json` file.
+
+## Latest Release
+
+The latest version of the module is v0.5.0 `BETA`.
+
+## Usage
+
+All controllers or base controller:
+
+```php
+use keygenqt/lastPage/LastPage
+
+class BaseController extends LastPage
+{
+	...
+```
+
+Get url:
+
+```php
+Yii::$app->user->getReturnUrl()
+```
+
+## License
+
+**yii2-last-page** is released under the BSD 3-Clause License. See the bundled `LICENSE.md` for details.
+
+
