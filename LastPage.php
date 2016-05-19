ad4e839823b599a8eedcdc8b5d6fc14bc7ebeb28
KeyGenQt <keygenqt@gmail.com>
2016-05-19 19:46:32 +0300

    fix [ajax]

--- LastPage.php
+++ LastPage.php
@@ -15,7 +15,7 @@ class LastPage extends Controller
         $result = parent::beforeAction($action);
         if (empty($_FILES)) {
             $request = Yii::$app->request;
-            if ((!$request->isAjax || !empty($_GET['ajax'])) && strpos($request->getUrl(), '.') === false) {
+            if (!$request->isAjax && strpos($request->getUrl(), '.') === false && empty($_GET['ajax'])) {
                 if (($oldUrl = Yii::$app->session->get('user.url')) !== null) {
                     if (($newUrl = Yii::$app->session->get('user.new-url')) != $request->getUrl()) {
                         Yii::$app->session->set('user.new-url', $request->getUrl());
